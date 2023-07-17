46889fba637d31dadcae655b102d4df93c3a9a0b
Yiler Huang <102222216+Yiler7274@users.noreply.github.com>
2023-07-17 11:49:16 +0800

    Update arrayToolkit.js
    updated top comment

--- arrayToolkit.js
+++ arrayToolkit.js
@@ -1,10 +1,11 @@
 // --------------------------------------------------------------------------
-// -- utilities-array.js
+// -- arrayToolkit.js
 // -- initial author: Renick Bell (renick@gmail.com)
 // -- initial creation date: Fri Jul 7 10:57 AM CST 2023
 // -- contributors: Yiler Huang (yiler7777@gmail.com); Steve Wang (stevesg168@gmail.com)
 // -- license: GPL 3.0
 // --------------------------------------------------------------------------
+
 function buildArray(n, fillFunction) {
     let outputArray = [];
     for (let i = 0; i < n; i++) {
