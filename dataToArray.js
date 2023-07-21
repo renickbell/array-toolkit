ceaedbcbf562af7810f0549d76a3f09a9b6258b1
mrname5 <122348110+mrname5@users.noreply.github.com>
2023-07-21 21:19:59 +0800

    Update dataToArray.js
    Removed these functions form arrayToolkit.js and moved them here.

--- dataToArray.js
+++ dataToArray.js
@@ -1,1 +1,38 @@
+// --------------------------------------------------------------------------
+// -- dataToArray.js
+// -- initial author: Renick Bell (renick@gmail.com)
+// -- initial creation date: Wed Jun 28 10:08:48 AM CST 2023
+// -- contributors: Yiler Huang (yiler7777@gmail.com); Steve Wang (stevesg168@gmail.com)
+// -- license: GPL 3.0
+// --------------------------------------------------------------------------
 
+function buildArray (n, fillFunction) {
+  let outputArray = [];
+  for (let i = 0; i < n; i++) {
+    outputArray.push(fillFunction(i))
+  }
+  return outputArray
+}
+
+function numArray (start,end) {
+    let output = [];
+    for(let i = start; i <= end; i++) {output.push(i)};
+    return output
+}
+
+// modify so that it doesn't have crash potential
+function linearArray (start, step, end) {
+    let output = [start];
+    for (let i = 1; (start + (i*step)) <= end; i++) {
+        output.push(start + (i*step))
+    }
+    return output
+}
+
+function geometricArray (start, step, end) {
+    let output = [start];
+    for (let i = 1; (start * (i*step)) <= end; i++) {
+        output.push(start * (i*step))
+    }
+    return output
+}
