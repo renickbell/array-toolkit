fc71a41e06a96f2ea9f8d1a42419e87c0f5942b1
Yiler Huang <102222216+Yiler7274@users.noreply.github.com>
2023-07-27 11:04:43 +0800

    Update dataToArray.js
    added comments for all the functions

--- dataToArray.js
+++ dataToArray.js
@@ -6,6 +6,7 @@
 // -- license: GPL 3.0
 // --------------------------------------------------------------------------
 
+//Takes a function and make the outputs from it an array, the function can use the current index as an argument.
 function buildArray (n, fillFunction) {
   let outputArray = [];
   for (let i = 0; i < n; i++) {
@@ -23,10 +24,12 @@ function linearArray (start, step, end) {
     return output
 }
 
+//Makes an array filled with integers, the starting and anding value can be specified.
 function integerArray (start,end) {
     return linearArray(start, 1, end)
 }
 
+//makes an array of numbers, you can specify the gap between each number
 function geometricArray (start, step, end) {
     let output = [start];
     for (let i = 1; (start * (i*step)) <= end; i++) {
