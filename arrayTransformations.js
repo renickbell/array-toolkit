03198e7f043e29c29d15777c4a4d13178beca479
Renick Bell <renick@gmail.com>
2023-08-02 11:37:59 +0800

    replaced shuffle with functional version
    the old shuffle used while. this version uses functional techniques instead.

--- arrayTransformations.js
+++ arrayTransformations.js
@@ -126,17 +126,11 @@ function buildZip(a, b) {
 }
 
 function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-  return array;
+  return array.reduceRight((acc, _, currentIndex) => {
+    const randomIndex = Math.floor(Math.random() * (currentIndex + 1));
+    [acc[currentIndex], acc[randomIndex]] = [acc[randomIndex], acc[currentIndex]];
+    return acc;
+  }, [...array]);
 }
 
 function gatherBySubstring (inputArray, substringArray) {
