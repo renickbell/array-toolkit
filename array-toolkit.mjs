47b48d4886bc41c4121bd2872e80d0c15ad29a96
mrname5 <stevesg168@gmail.com>
2023-08-08 19:02:31 +0800

    Changed version number and fixed file paths for array-toolkit.mjs

--- array-toolkit.mjs
+++ array-toolkit.mjs
@@ -10,6 +10,6 @@ const R = require('ramda')
 const fs = require('fs');
 const path = require('path');
 
-export * from './node_modules/array-toolkit/arrayTransformations.mjs'
-export * from './node_modules/array-toolkit/dataToArray.mjs'
-export * from './node_modules/array-toolkit/infoFromArray.mjs'
+export * from './arrayTransformations.mjs'
+export * from './dataToArray.mjs'
+export * from './infoFromArray.mjs'
