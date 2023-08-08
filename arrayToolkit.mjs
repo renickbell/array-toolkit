b8eee6c75795811545f76aa1faceb3b0f69adf08
mrname5 <stevesg168@gmail.com>
2023-08-08 18:47:40 +0800

    Fixed file extenstion for the loaded modules

--- arrayToolkit.mjs
+++ arrayToolkit.mjs
@@ -1,5 +1,5 @@
 // --------------------------------------------------------------------------
-// -- arrayToolkit.js
+// -- arrayToolkit.mjs
 // -- initial author: Renick Bell (renick@gmail.com)
 // -- initial creation date: Wed Jun 28 10:08:48 AM CST 2023
 // -- contributors: Yiler Huang (yiler7777@gmail.com); Steve Wang (stevesg168@gmail.com)
@@ -10,6 +10,6 @@ const R = require('ramda')
 const fs = require('fs');
 const path = require('path');
 
-export * from './node_modules/array-toolkit/arrayTransformations.js'
-export * from './node_modules/array-toolkit/dataToArray.js'
-export * from './node_modules/array-toolkit/infoFromArray.js'
+export * from './node_modules/array-toolkit/arrayTransformations.mjs'
+export * from './node_modules/array-toolkit/dataToArray.mjs'
+export * from './node_modules/array-toolkit/infoFromArray.mjs'
