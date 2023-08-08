3443ffd7f4e57a70c3f4c1fb61dccee44acbc34d
mrname5 <stevesg168@gmail.com>
2023-08-08 16:39:52 +0800

    Modified to use import

---
+++ arrayToolkit.mjs
@@ -0,0 +1,15 @@
+// --------------------------------------------------------------------------
+// -- arrayToolkit.js
+// -- initial author: Renick Bell (renick@gmail.com)
+// -- initial creation date: Wed Jun 28 10:08:48 AM CST 2023
+// -- contributors: Yiler Huang (yiler7777@gmail.com); Steve Wang (stevesg168@gmail.com)
+// -- license: GPL 3.0
+// --------------------------------------------------------------------------
+
+const R = require('ramda')
+const fs = require('fs');
+const path = require('path');
+
+export * from './node_modules/array-toolkit/arrayTransformations.js'
+export * from './node_modules/array-toolkit/dataToArray.js'
+export * from './node_modules/array-toolkit/infoFromArray.js'
